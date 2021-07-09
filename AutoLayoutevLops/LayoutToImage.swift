b4bacffd8c9bbf9703bb324c0344f767aa60e35a
iOSayedAhmed <elsayed1ahmed0@gmail.com>
2021-07-09 15:35:45 +0200

    Initial Commit

---
+++ AutoLayoutevLops/LayoutToImage.swift
@@ -0,0 +1,9 @@
+
+import UIKit
+class image : UIImageView {
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 12
+        
+    }
+    
+}
---
+++ AutoLayoutevLops/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutevLops
+//
+//  Created by Develop on 10/12/19.
+//  Copyright © 2019 Develop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
